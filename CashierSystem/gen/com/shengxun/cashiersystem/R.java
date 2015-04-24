6ee8f656096a2286da9d13b7a92c9ff18f35e376
LILINQ <LILINQ@PC201402241028>
2015-04-24 10:05:21 +0800

    应用Application修改

--- CashierSystem/gen/com/shengxun/cashiersystem/R.java
+++ CashierSystem/gen/com/shengxun/cashiersystem/R.java
@@ -90,31 +90,30 @@ public final class R {
         public static final int logo_144=0x7f020001;
     }
     public static final class id {
-        public static final int cashier_listview=0x7f080001;
-        public static final int cashier_system_business=0x7f080003;
-        public static final int cashier_system_column_cargo_number=0x7f08000d;
-        public static final int cashier_system_column_clerk=0x7f080014;
-        public static final int cashier_system_column_code=0x7f08000e;
-        public static final int cashier_system_column_count=0x7f080010;
-        public static final int cashier_system_column_money=0x7f080013;
-        public static final int cashier_system_column_name=0x7f08000f;
-        public static final int cashier_system_column_now_price=0x7f080012;
-        public static final int cashier_system_column_old_price=0x7f080011;
-        public static final int cashier_system_column_sort=0x7f08000c;
+        public static final int cashier_listview=0x7f080003;
+        public static final int cashier_system_business=0x7f080001;
+        public static final int cashier_system_column_cargo_number=0x7f08000c;
+        public static final int cashier_system_column_clerk=0x7f080013;
+        public static final int cashier_system_column_code=0x7f08000d;
+        public static final int cashier_system_column_count=0x7f08000f;
+        public static final int cashier_system_column_money=0x7f080012;
+        public static final int cashier_system_column_name=0x7f08000e;
+        public static final int cashier_system_column_now_price=0x7f080011;
+        public static final int cashier_system_column_old_price=0x7f080010;
+        public static final int cashier_system_column_sort=0x7f08000b;
         public static final int cashier_system_machine_number=0x7f080005;
         public static final int cashier_system_machine_order_receive=0x7f080009;
         public static final int cashier_system_machine_order_return_balance=0x7f080007;
         public static final int cashier_system_machine_order_total=0x7f080006;
         public static final int cashier_system_machine_status=0x7f080008;
-        public static final int cashier_system_machine_time=0x7f08000b;
+        public static final int cashier_system_machine_time=0x7f080000;
         public static final int cashier_system_machine_user=0x7f08000a;
-        public static final int help_fast_key_linearlayout=0x7f080002;
-        public static final int help_fast_key_linearlayout_v_line=0x7f080000;
+        public static final int help_fast_key_linearlayout_v_line=0x7f080002;
         public static final int relativelayout_horizontal_center_line=0x7f080004;
-        public static final int user_login=0x7f080017;
-        public static final int user_name=0x7f080015;
-        public static final int user_password=0x7f080016;
-        public static final int user_reset=0x7f080018;
+        public static final int user_login=0x7f080016;
+        public static final int user_name=0x7f080014;
+        public static final int user_password=0x7f080015;
+        public static final int user_reset=0x7f080017;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -124,11 +123,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int cashier_system_alert_login_fail=0x7f05001b;
-        public static final int cashier_system_alert_login_no_permissions=0x7f05001c;
+        public static final int cashier_system_alert_login_fail=0x7f05001e;
+        public static final int cashier_system_alert_login_no_permissions=0x7f05001f;
         /**  提示语 
          */
-        public static final int cashier_system_alert_no_login=0x7f05001a;
+        public static final int cashier_system_alert_no_login=0x7f05001d;
         public static final int cashier_system_business=0x7f050013;
         public static final int cashier_system_column_cargo_number=0x7f05000b;
         public static final int cashier_system_column_clerk=0x7f050012;
@@ -151,6 +150,9 @@ public final class R {
         public static final int cashier_system_machine_order_total=0x7f050016;
         public static final int cashier_system_machine_status=0x7f050015;
         public static final int cashier_system_machine_user=0x7f050019;
+        public static final int cashier_system_open_cashbox=0x7f05001b;
+        public static final int cashier_system_receive_payments=0x7f05001a;
+        public static final int cashier_system_setting=0x7f05001c;
         public static final int cashier_system_title=0x7f050007;
         public static final int hello_world=0x7f050002;
         public static final int user_login=0x7f050005;
--- CashierSystem/src/com/shengxun/cashiersystem/app/ApplicationCS.java
+++ CashierSystem/src/com/shengxun/cashiersystem/app/ApplicationCS.java
@@ -1,6 +1,11 @@
 package com.shengxun.cashiersystem.app;
 
+import net.tsz.afinal.http.AjaxCallBack;
+
 import com.shengxun.entity.LoginInfo;
+import com.shengxun.util.ConnectManager;
+import com.zvezda.android.utils.LG;
+import com.zvezda.database.utils.ORMOpearationDao;
 
 import android.app.Application;
 
@@ -12,9 +17,30 @@ import android.app.Application;
 public class ApplicationCS extends Application{
 
 	public LoginInfo loginInfo=null;
+	/**
+	 * ORM数据库操作封装
+	 */
+	protected ORMOpearationDao ormOpearationDao=null;
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ormOpearationDao=new ORMOpearationDao(getApplicationContext());
+		LG.i(ApplicationCS.class, "收银系统启动------>1:获取联网最新产品信息");
+		ConnectManager.getInstance().getProductList(ajaxCallBack);
 	}
-  
+	private AjaxCallBack<String> ajaxCallBack=new AjaxCallBack<String>() {
+
+		@Override
+		public void onSuccess(String t) {
+			super.onSuccess(t);
+			LG.i(ApplicationCS.class, "收银系统启动------>2：将最新的数据写入数据库");
+		}
+
+		@Override
+		public void onFailure(Throwable t, int errorNo, String strMsg) {
+			super.onFailure(t, errorNo, strMsg);
+		}
+		
+		
+	};
 }
